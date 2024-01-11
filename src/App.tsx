5aa82212ad47e25d19a1d0fcc4f9ffb08c4b172f
unknown <tsyganov.mik@gmail.com>
2024-01-11 15:30:48 +0400

    1.6

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import { Counter } from "./components/Counter";
-
+import { Suspense } from "react";
 import "./index.scss";
+import { Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy";
+import { MainPageLazy } from "./pages/MainPage/MainPage.lazy";
 
 export default function App() {
   return (
     <div className="app">
-      dwadwadawdaw
-      <Counter />
+      <Link to="/">Главная</Link>
+      <Link to="/about">О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/about" element={<AboutPageLazy />} />
+          <Route path="/" element={<MainPageLazy />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
