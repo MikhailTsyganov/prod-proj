12f5ad49d407f37c0931508f89cb581f3b340a03
unknown <tsyganov.mik@gmail.com>
2024-01-18 11:36:50 +0400

    20

---
+++ src/shared/lib/helpers/classNames/classNames.test.ts
@@ -0,0 +1,39 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+  test('with only first param', () => {
+    expect(classNames('someClass')).toBe('someClass');
+  });
+
+  test('with additional param', () => {
+    const expected = 'someClass class1 class2';
+    expect(classNames('someClass', {}, ['class1', 'class2'])).toBe(expected);
+  });
+
+  test('with mods', () => {
+    const expected = 'someClass class1 class2 hovered scrollable';
+    expect(classNames(
+      'someClass',
+      { hovered: true, scrollable: true },
+      ['class1', 'class2']
+    )).toBe(expected);
+  });
+
+  test('with mods and one is false', () => {
+    const expected = 'someClass class1 class2 scrollable';
+    expect(classNames(
+      'someClass',
+      { hovered: false, scrollable: true },
+      ['class1', 'class2']
+    )).toBe(expected);
+  });
+
+  test('with mods and one is undefined', () => {
+    const expected = 'someClass class1 class2';
+    expect(classNames(
+      'someClass',
+      { hovered: false, scrollable: undefined },
+      ['class1', 'class2']
+    )).toBe(expected);
+  });
+})
