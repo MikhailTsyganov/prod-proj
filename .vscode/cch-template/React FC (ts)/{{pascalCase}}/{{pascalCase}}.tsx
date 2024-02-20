f7c75fd6d639f28856be06baeb69e79de3787b48
mixa <tsyganov.mik@gmail.com>
2024-02-20 10:11:08 +0400

    45 added npm concurrently, file templates

--- .vscode/cch-template/React FC (ts)/{{pascalCase}}/{{pascalCase}}.tsx
+++ .vscode/cch-template/React FC (ts)/{{pascalCase}}/{{pascalCase}}.tsx
@@ -1,6 +1,19 @@
-import { FC } from 'react';
-import { {{pascalCase}}Props } from '.';
+import { FC } from "react";
+import { classNames } from "shared/lib/helpers/classNames/classNames";
 
-export const {{pascalCase}}: FC<{{pascalCase}}Props> = (props) => {
-	return <div {...props}></div>;
+import s from "./{{pascalCase}}.module.scss";
+
+interface I{{pascalCase}}Props {
+	className ?: string;
+}
+
+export const {{pascalCase}}: FC <I{{pascalCase}}Props> = (props) => {
+	const { className, children } = props;
+
+	return (
+	<div className={classNames(s.{{pascalCase}}, {}, [className])}>
+		{children}
+	</div >
+  )
 };
+
